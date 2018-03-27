bb2c7b67f13af699e6981550784d0df55ad62eea
Tomas Zachoval <zachovaltomas@hotmail.com>
2018-03-27 12:48:38 +0200

    add colochosers

--- VektorovyEditor/ViewModel/MainViewModel.cs
+++ VektorovyEditor/ViewModel/MainViewModel.cs
@@ -1,12 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using VektorovyEditor.helpers;
+using Xceed.Wpf.Toolkit;
 
 namespace VektorovyEditor.ViewModel
 {
-    class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        public Color BorderColor
+        {
+            get => _borderColor;
+            set
+            {
+                _borderColor = value; 
+                OnPropertyChanged(nameof(BorderColor));
+            }
+        }
+
+        public Color FillColor
+        {
+            get => _fillColor;
+            set
+            {
+                _fillColor = value;
+                OnPropertyChanged(nameof(BorderColor));
+            }
+        }
+
+
+        public ZCommand SetBorderColorCommand { get; set; }
+
+
+        public MainViewModel()
+        {
+            BorderColor = Colors.BlueViolet;
+            FillColor = Colors.GreenYellow;
+        }
+
+        private void SetBorderColor(object obj)
+        {
+            
+        }
+
+        private bool CanBorder(object obj)
+        {
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Color _borderColor;
+        private Color _fillColor;
     }
 }
